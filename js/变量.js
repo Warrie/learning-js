84eb708c26123200f5e8df47195b71a5f704d61f
Warrie <warrie@163.com>
2018-04-20 14:57:52 +0800

    0419

---
+++ js/变量.js
@@ -0,0 +1,38 @@
+// function b(c){
+//     　　　　console.log(c);
+//     　　　　function c(){
+//     　　　　　　　console.log("d");
+//     　　　　}
+//     　　}
+//     　　b(10);
+
+
+// console.log(e);
+// var e=function(aa){
+//     aa;
+// }
+
+// console.log(f);
+// var f = 1;
+// function f(){
+//     console.log('foodoir');
+// }
+
+// console.log(f);
+// function f(){
+//     console.log('foodoir');
+// }
+// var f = 1;
+
+// console.log(f);
+// var f = 1;
+// var f = 2;
+
+
+console.log(f.toString());
+function f(){
+    console.log('foodoir');
+}
+function f(){
+    console.log('hello world');
+}
---
+++ 函数声明与变量重名.js
@@ -0,0 +1,15 @@
+// var a=1;
+// function a(){
+//     console.log(10);
+// }
+// console.log(a);
+// a();
+
+var a=4;
+function b() {
+         a=3; 
+   console.log(a); 
+   function a(){};
+  }
+ b();
+ console.log(a)
